92a9a7082897bc134ae02cd8495751e2ace103af
清水雄大 <shimizuyuta@Yutas.local>
2020-02-12 13:12:02 +0900

    requestsテスト追加

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,8 +1,14 @@
 require 'rails_helper'
-
 RSpec.describe "Posts", type: :request do
   describe "GET /posts" do
-    it "works! (now write some real specs)" do
+    FactoryBot.create :topic
+
+    before do
+      get posts_path
+    end
+
+    it { expect(response.status).to eq 200 }
+
       get posts_index_path
       expect(response).to have_http_status(200)
     end
